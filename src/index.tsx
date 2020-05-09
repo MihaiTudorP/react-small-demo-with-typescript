d01bd334588b83ed2b2c35da701c5b260fcfe8f9
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-05-09 21:35:26 +0300

    Finished app

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {App} from './components/App';
+import {reducers} from './reducers';
 
-interface AppProps {
-    color?: string;
-}
-
-interface AppState {
-    counter: number;
-}
-
-class App extends React.Component<AppProps, AppState>{
-    constructor(props: AppProps) {
-        super(props);
-
-        this.state = {counter: 0};
-    }
-
-    onIncrement = (): void =>{
-        this.setState({counter: this.state.counter + 1})
-    };
-
-    onDecrement = (): void =>{
-        this.setState({counter: this.state.counter - 1})
-    };
-
-    render() {
-        return <div>
-            <h1>{this.state.counter}</h1>
-            <button onClick={this.onIncrement}>Increment</button>
-            <button onClick={this.onDecrement}>Decrement</button>
-        </div>;
-    }
-}
-
-ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+    );
