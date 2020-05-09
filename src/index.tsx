2e41100a6af727211932ac184d6009180bb8dd20
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-05-09 16:59:40 +0300

    Added initial behaviour

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface AppProps {
+    color?: string;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component<AppProps>{
+    state = { counter: 0 }
+
+    onIncrement = (): void =>{
+        this.setState({counter: this.state.counter + 1})
+    };
+
+    onDecrement = (): void =>{
+        this.setState({counter: this.state.counter - 1})
+    };
+
+    render() {
+        return <div>
+            <h1>{this.state.counter}</h1>
+            <button onClick={this.onIncrement}>Increment</button>
+            <button onClick={this.onDecrement}>Decrement</button>
+        </div>;
+    }
+}
+
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
